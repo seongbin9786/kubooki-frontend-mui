5c3fb1ba2c82805dd8ba824984b25d3b2ff7f5c0
Seongbin Kim <seongbin9786@gmail.com>
2018-05-17 16:16:50 +0900

    로그인 폼 레이아웃 구현 closes #5

---
+++ src/LoginDialog.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export default class extends Component {
+  state = {
+    id: '',
+    password: '',
+  };
+
+  handleChange = inputName => ({ target: { value } }) =>
+    this.setState({ [inputName]: value });
+
+  render() {
+    const { open, handleClose, onSubmit, onRegisterClick } = this.props;
+    const { id, password } = this.state;
+
+    return (
+      <div>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">로그인</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="id"
+              label="아이디"
+              value={id}
+              onChange={this.handleChange('id')}
+              type="text"
+              fullWidth
+            />
+            <TextField
+              margin="dense"
+              id="password"
+              label="비밀번호"
+              value={password}
+              onChange={this.handleChange('password')}
+              type="password"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={onRegisterClick} color="primary">
+              아이디가 없으신가요? 회원가입
+            </Button>
+            <Button onClick={handleClose} color="primary">
+              취소
+            </Button>
+            <Button onClick={onSubmit} color="primary" variant='raised'>
+              로그인
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
