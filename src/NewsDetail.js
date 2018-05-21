6dc8f6baa19b62dc81c8a1cc331d63568048f669
Seongbin Kim <seongbin9786@gmail.com>
2018-05-21 09:41:49 +0900

    기사 읽기 레이아웃 생성 closes #21

--- src/NewsDetail.js
+++ src/NewsDetail.js
@@ -1,108 +1,143 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
 
-export default () => (
-  <article className="post theme--section-99">
+import { newsDetail as news, newsDetailPageParagraphs, newsDetailpageImages } from './store';
 
-    <header className="post__header">
-      <ul className="post__categories">
-        <li className="post__category"><a className="theme__link" href="/inspiration/">Inspiration</a> /</li>
-        <li className="post__category"><a className="theme__link" href="/inspiration/photography/">Photography</a></li>
-      </ul>
-      <h1 className="post__title">
-        Raphael Olivier's stunning photographs unveil the unique architecture of North Korea's secretive capital
-      </h1>
-      <p className="post__author">
-        <span className="post__author-intro">Written by</span>
-        <a className="post__author-link theme__link" href="/contributors/katy-cowan/">Katy Cowan</a>
+const styles = theme => ({
+  article: {
+    padding: '0 20px',
+    paddingTop: '100px',
+  },
+  header: {
+    textAlign: 'center',
+  },
+  yellowHighlight: {
+    display: 'inline-block',
+    borderBottom: '2px #ECCA30 solid',
+    color: '#000',
+    textDecoration: 'none',
+  },
+  title: {
+    margin: '0 auto 40px',
+    marginTop: '60px',
+    marginBottom: '60px',
+    fontSize: '30px',
+    lineHeight: '38px',
+  },
+  writtenBy: {
+    display: 'block',
+  },
+  author: {
+    display: 'inline-block',
+    borderBottom: '2px #ECCA30 solid',
+    color: '#000',
+    textDecoration: 'none',
+  },
+  figure: {
+    margin: '80px auto',
+    marginBottom: '60px',
+    maxWidth: '1000px !important',
+  },
+  img: {
+    maxWidth: '100%',
+  },
+  figCaption: {
+    maxWidth: '1100px',
+  },
+  body: {
+    position: 'relative',
+  },
+  paragraph: {
+    marginBottom: '28px',
+    maxWidth: '700px',
+    margin: '0 auto',
+  },
+  footer: {
+    margin: '80px 0 60px',
+    textAlign: 'center',
+  },
+  date: {
+    display: 'block',
+    marginTop: '20px',
+    color: '#b2b2b2',
+  }
+});
+
+export default withStyles(styles)(({ classes }) => (
+  <article className={classes.article}>
+
+    <header className={classes.header}>
+      <Link to="/" className={classes.yellowHighlight}>{news.category}</Link>
+      <Typography
+        className={classes.title}
+        variant="display2"
+      >
+        {news.title}
+      </Typography>
+      <p>
+        <span className={classes.writtenBy}>작성자</span>
+        <Link className={classes.author} to="/">{news.writer}</Link>
       </p>
     </header>
 
-    <section className="post__content">
+    <section>
 
-      <figure className="post__lead-image-container">
-        <img className="post__lead-image" src="https://www.creativeboom.com/uploads/articles/2b/2be47fa48493c19a47bb27edee9d03e4c0335b04_1100.jpg" />
-        <figcaption style={{ maxWidth: '1100px' }}><p>© Raphael Olivier</p>
+      <figure className={classes.figure}>
+        <img
+          className={classes.img}
+          src="https://www.creativeboom.com/uploads/articles/2b/2be47fa48493c19a47bb27edee9d03e4c0335b04_1100.jpg"
+          alt="© 김성빈"
+          width="1100"
+        />
+        <figcaption className={classes.figCaption}>
+          <p style={{ margin: 0 }}>© 김성빈</p>
         </figcaption>
       </figure>
 
+      <div className={classes.body}>
+        {newsDetailPageParagraphs.map((content, index) => (
+          <p className={classes.paragraph} key={index} dangerouslySetInnerHTML={{ __html: content }}></p>
+        ))}
 
-      <div className="post__body">
-        <p>When French photographer <a href="http://www.raphaelolivier.com/">Raphael Olivier</a> decided to make a flying visit to North Korea in 2015, he had no expectations about the secretive nation; he was living in nearby Shanghai at the time and just happened to find a cheap ticket online and thought, why not? After just 24 hours wandering around Pyongyang, he was hooked.</p>
-        <p>"This express dip into the 'North' suddenly opened my eyes beyond anything I would have ever imagined," Raphael told Creative Boom. "The place actually fascinated me, everything about it was amazing. So much so that the next year, I decided to return for a longer trip in order to get a chance to see and understand more about this country so often misjudged."</p>
-        <p>One of the most obvious aspects of Pyongyang which immediately caught his attention was its "retro vibe and architecture". Which is why, during his second visit, he aimed to photograph its unique urban landscape. Through this series, many of the buildings he captured have a very strong Soviet modernist influence with a distinctive Korean style of their own. </p>
-        <p>"As opposed to many cities, which are now starting to look all the same worldwide, Pyongyang sees very little advertising or visual pollution, offering the visitor a very raw display of its urban form," Raphael adds. "To this day, Pyongyang is still one the most memorable places I have ever visited and, regardless of anyone's opinion on political or economic matters, I think it's fair to say that everyone visiting Pyongyang is left impressed by the city's strong and proud character.</p>
-        <p>"My images are only a glimpse into a world that seems so distant yet so real, but I encourage anyone curious about the DPRK to go and visit to make an idea for themselves." Discover more at <a href="http://www.raphaelolivier.com/urban/documentary/photographer/north-korea/pyongyang/vintage/socialist/architecture/">raphaelolivier.com</a>.</p>
-
-        <figure className="post__body-image">
-          <img src="https://www.creativeboom.com/uploads/articles/ea/eabd38c091d188b6fd489ee05e46ebd5e7cd5a3b_1100.jpg" alt="© Raphael Olivier" width="1100" />
-          <figcaption style={{ maxWidth: '1100px' }}>
-            <p>© Raphael Olivier</p>
-          </figcaption>
-        </figure>
-
-        <figure className="post__body-image">
-          <img src="https://www.creativeboom.com/uploads/articles/05/05b6eb79468f927933dd03cde0542d987923b37d_1100.jpg" alt="© Raphael Olivier" width="1100" />
-          <figcaption style={{ maxWidth: '1100px' }}>
-            <p>© Raphael Olivier</p>
-          </figcaption>
-        </figure>
-
-        <figure className="post__body-image">
-          <img src="https://www.creativeboom.com/uploads/articles/13/137068c7b9999e1d3896be80489ca4e5c2cbc3fd_1100.jpg" alt="© Raphael Olivier" width="1100" />
-          <figcaption style={{ maxWidth: '1100px' }}>
-            <p>© Raphael Olivier</p>
-          </figcaption>
-        </figure>
-
-        <figure className="post__body-image"><img src="https://www.creativeboom.com/uploads/articles/d0/d0d9bfee9be48bfb8c48d9542ac4587d878eb163_1100.jpg" alt="© Raphael Olivier" width="1100" /><figcaption style={{ maxWidth: '1100px' }}><p>© Raphael Olivier</p>
-        </figcaption></figure>
-
-        <figure className="post__body-image"><img src="https://www.creativeboom.com/uploads/articles/d3/d3fc5dad8dc02436fcf204469355b3c771c94402_1100.jpg" alt="© Raphael Olivier" width="1100" /><figcaption style={{ maxWidth: '1100px' }}><p>© Raphael Olivier</p>
-        </figcaption></figure>
-
-        <figure className="post__body-image"><img src="https://www.creativeboom.com/uploads/articles/10/1087391e8fa7196b091ef219c859d26a28a84b40_1100.jpg" alt="© Raphael Olivier" width="1100" /><figcaption style={{ maxWidth: '1100px' }}><p>© Raphael Olivier</p>
-        </figcaption></figure>
-
-        <figure className="post__body-image"><img src="https://www.creativeboom.com/uploads/articles/6c/6cd1f8b0e5c7be298ab56420ae842d91e8c761cb_1100.jpg" alt="© Raphael Olivier" width="1100" /><figcaption style={{ maxWidth: '1100px' }}><p>© Raphael Olivier</p>
-        </figcaption></figure>
-
-        <figure className="post__body-image"><img src="https://www.creativeboom.com/uploads/articles/bd/bdfd408afb7bc318e152cf5cb07b0b0add3cc2d7_1100.jpg" alt="© Raphael Olivier" width="1100" /><figcaption style={{ maxWidth: '1100px' }}><p>© Raphael Olivier</p>
-        </figcaption></figure>
-
-        <figure className="post__body-image"><img src="https://www.creativeboom.com/uploads/articles/17/17cf1e9c76e1fac17547ed8ebe4aedf6053fe98f_1100.jpg" alt="© Raphael Olivier" width="1100" /><figcaption style={{ maxWidth: '1100px' }}><p>© Raphael Olivier</p>
-        </figcaption></figure>
-
-        <figure className="post__body-image"><img src="https://www.creativeboom.com/uploads/articles/2d/2d72e08c9215f58273c770041c4e8d729a0f6882_1100.jpg" alt="© Raphael Olivier" width="1100" /><figcaption style={{ maxWidth: '1100px' }}><p>© Raphael Olivier</p>
-        </figcaption></figure>
-
-        <figure className="post__body-image"><img src="https://www.creativeboom.com/uploads/articles/e8/e89172119556fa2cd44205b1f7fc3218546ea517_1100.jpg" alt="© Raphael Olivier" width="1100" /><figcaption style={{ maxWidth: '1100px' }}><p>© Raphael Olivier</p>
-        </figcaption></figure>
-
-        <figure className="post__body-image"><img src="https://www.creativeboom.com/uploads/articles/b2/b2b98457b1f500734675ba83f1ba1b37a9dffc11_1100.jpg" alt="© Raphael Olivier" width="1100" /><figcaption style={{ maxWidth: '1100px' }}><p>© Raphael Olivier</p>
-        </figcaption></figure>
-
-        <figure className="post__body-image"><img src="https://www.creativeboom.com/uploads/articles/d6/d6a4fd8c121eeae92d1d346d1b118403cc0ebb0d_1100.jpg" alt="© Raphael Olivier" width="1100" /><figcaption style={{ maxWidth: '1100px' }}><p>© Raphael Olivier</p>
-        </figcaption></figure>
+        {newsDetailpageImages.map((imgUrl, index) => (
+          <figure className={classes.figure} key={index}>
+            <img
+              className={classes.img}
+              src={imgUrl}
+              alt="© 김성빈"
+              width="1100"
+            />
+            <figcaption className={classes.figCaption}>
+              <p style={{ margin: 0 }}>© 김성빈</p>
+            </figcaption>
+          </figure>
+        ))}
 
       </div>
 
     </section>
 
-    <footer className="post__footer">
+    <footer className={classes.footer}>
 
       <p className="post__author">
-        <span className="post__author-intro">Written by</span>
-        <a className="post__author-link theme__link" href="/contributors/katy-cowan/">Katy Cowan</a>
+        <span className={classes.writtenBy}>작성자</span>
+        <Link className={classes.yellowHighlight} to="/">{news.writer}</Link>
       </p>
-      <time className="post__date">18.05.2018</time>
-
-      <div className="post__social">
-        <a rel="external" className="social-link social-link--twitter" href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fwww.creativeboom.com%2Finspiration%2Fraphael-oliviers-photographs-unveil-the-unique-architecture-of-north-koreas-secretive-capital%2F&amp;text=Raphael+Olivier%27s+stunning+photographs+unveil+the+unique+architecture+of+North+Korea%27s+secretive+capital&amp;via=CreativeBoomMag">Twitter</a>
-        <a rel="external" className="social-link social-link--pinterest" href="https://pinterest.com/pin/create/bookmarklet/?is_video=false&amp;url=https%3A%2F%2Fwww.creativeboom.com%2Finspiration%2Fraphael-oliviers-photographs-unveil-the-unique-architecture-of-north-koreas-secretive-capital%2F">Pinterest</a>
-        <a rel="external" className="social-link social-link--facebook" href="https://www.facebook.com/sharer.php?u=https%3A%2F%2Fwww.creativeboom.com%2Finspiration%2Fraphael-oliviers-photographs-unveil-the-unique-architecture-of-north-koreas-secretive-capital%2F">Facebook</a>
-      </div>
+      <time className={classes.date}>{news.lastUpdatedDate}</time>
 
     </footer>
 
   </article>
-);
+
+  // --------------------------------------------------
+  // TODO: 명함 영역
+
+  // --------------------------------------------------
+  // TODO: 댓글 영역 (댓글 불러오기 버튼)
+
+  // --------------------------------------------------
+  // TODO: 같은 분류의 기사 보기
+
+));
--- src/store.js
+++ src/store.js
@@ -49,3 +49,30 @@ export const newsList = [
     'img': 'https://www.creativeboom.com/uploads/articles/bc/bcb8dc807536164d94e7d9f77e22728c970d1bee_1100.jpg',
   },
 ];
+
+export const newsDetail = {
+  'category': '경기소식',
+  'title': '경기대학교 신입생들을 위한 새내기팁! 첫 번째, 통학버스와 고양이버스 안내',
+  'writer': '김성빈',
+  'lastUpdatedDate': '18.05.2018',
+};
+
+export const newsDetailPageParagraphs = [
+  'When French photographer <a href="http://www.raphaelolivier.com/">Raphael Olivier</a> decided to make a flying visit to North Korea in 2015, he had no expectations about the secretive nation; he was living in nearby Shanghai at the time and just happened to find a cheap ticket online and thought, why not? After just 24 hours wandering around Pyongyang, he was hooked.',
+  '"This express dip into the \'North\' suddenly opened my eyes beyond anything I would have ever imagined," Raphael told Creative Boom. "The place actually fascinated me, everything about it was amazing. So much so that the next year, I decided to return for a longer trip in order to get a chance to see and understand more about this country so often misjudged."',
+  'One of the most obvious aspects of Pyongyang which immediately caught his attention was its "retro vibe and architecture". Which is why, during his second visit, he aimed to photograph its unique urban landscape. Through this series, many of the buildings he captured have a very strong Soviet modernist influence with a distinctive Korean style of their own.',
+  '"As opposed to many cities, which are now starting to look all the same worldwide, Pyongyang sees very little advertising or visual pollution, offering the visitor a very raw display of its urban form," Raphael adds. "To this day, Pyongyang is still one the most memorable places I have ever visited and, regardless of anyone\'s opinion on political or economic matters, I think it\'s fair to say that everyone visiting Pyongyang is left impressed by the city\'s strong and proud character.',
+  '"My images are only a glimpse into a world that seems so distant yet so real, but I encourage anyone curious about the DPRK to go and visit to make an idea for themselves." Discover more at <a href="http://www.raphaelolivier.com/urban/documentary/photographer/north-korea/pyongyang/vintage/socialist/architecture/">raphaelolivier.com</a>.'
+];
+
+export const newsDetailpageImages = [
+  'https://www.creativeboom.com/uploads/articles/d3/d3fc5dad8dc02436fcf204469355b3c771c94402_1100.jpg',
+  'https://www.creativeboom.com/uploads/articles/10/1087391e8fa7196b091ef219c859d26a28a84b40_1100.jpg',
+  'https://www.creativeboom.com/uploads/articles/6c/6cd1f8b0e5c7be298ab56420ae842d91e8c761cb_1100.jpg',
+  'https://www.creativeboom.com/uploads/articles/bd/bdfd408afb7bc318e152cf5cb07b0b0add3cc2d7_1100.jpg',
+  'https://www.creativeboom.com/uploads/articles/17/17cf1e9c76e1fac17547ed8ebe4aedf6053fe98f_1100.jpg',
+  'https://www.creativeboom.com/uploads/articles/2d/2d72e08c9215f58273c770041c4e8d729a0f6882_1100.jpg',
+  'https://www.creativeboom.com/uploads/articles/e8/e89172119556fa2cd44205b1f7fc3218546ea517_1100.jpg',
+  'https://www.creativeboom.com/uploads/articles/b2/b2b98457b1f500734675ba83f1ba1b37a9dffc11_1100.jpg',
+  'https://www.creativeboom.com/uploads/articles/d6/d6a4fd8c121eeae92d1d346d1b118403cc0ebb0d_1100.jpg'
+];
