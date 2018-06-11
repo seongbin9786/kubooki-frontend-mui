0b0df40909c72e7e232f4b1e08f306e9b3a55db7
Seongbin Kim <seongbin9786@gmail.com>
2018-06-11 13:33:52 +0900

    ThemeProvider 추가, 임시 테마 적용

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import { create } from 'jss';
 import jssExtend from 'jss-extend';
 import JssProvider from 'react-jss/lib/JssProvider';
-import { jssPreset } from '@material-ui/core/styles';
+import { jssPreset, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import Layout from './Layout';
 import MainPage from './MainPage';
@@ -25,27 +25,40 @@ import AnnouncementReadPage from './AnnouncementReadPage';
 // jssExtend가 먼저오지 않으면 jss-camel-case를 원래 CSS 이름으로 변경해주지 않음
 const jss = create({ plugins: [jssExtend(), ...jssPreset().plugins] });
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#5b80b2'
+    },
+  },
+  typography: {
+    fontFamily: '\'Nanum Square\', sans-serif',
+  }
+});
+
 ReactDOM.render(
   <JssProvider jss={jss}>
-    <BrowserRouter>
-      <Layout>
-        <Switch>
-          <Route path='/announcements/:id' component={AnnouncementReadPage} />
-          <Route path='/announcements' component={AnnouncementPage} />
-          <Route path='/logs/meeting/:id' component={MeetingLogReadPage} />
-          <Route path='/logs/meeting' component={MeetingLogPage} />
-          <Route path='/account' component={MyPage} />
-          <Route path='/dashboard' component={Dashboard} />
-          <Route path='/terms' component={TermsOfService} />
-          <Route path='/faq' component={FAQPage} />
-          <Route path='/events/:id' component={EventDetailPage} />
-          <Route path='/events' component={EventPage} />
-          <Route path='/news/write' component={NewsWritePage} />
-          <Route path='/news' component={NewsDetail} />
-          <Route path='/' component={MainPage} />
-        </Switch>
-      </Layout>
-    </BrowserRouter>
+    <MuiThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route path='/announcements/:id' component={AnnouncementReadPage} />
+            <Route path='/announcements' component={AnnouncementPage} />
+            <Route path='/logs/meeting/:id' component={MeetingLogReadPage} />
+            <Route path='/logs/meeting' component={MeetingLogPage} />
+            <Route path='/account' component={MyPage} />
+            <Route path='/dashboard' component={Dashboard} />
+            <Route path='/terms' component={TermsOfService} />
+            <Route path='/faq' component={FAQPage} />
+            <Route path='/events/:id' component={EventDetailPage} />
+            <Route path='/events' component={EventPage} />
+            <Route path='/news/write' component={NewsWritePage} />
+            <Route path='/news' component={NewsDetail} />
+            <Route path='/' component={MainPage} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+    </MuiThemeProvider>
   </JssProvider>,
   document.getElementById('root')
 );
